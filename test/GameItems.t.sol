cb37d92ca6319fbbd8d2113eacad59e5b14a8486
Weiss1441 <nurymmuratov1441@gmail.com>
2026-05-15 14:55:05 +0500

    Update tests and config files

--- test/GameItems.t.sol
+++ test/GameItems.t.sol
@@ -10,57 +10,87 @@ contract GameItemsTest is Test {
     GameParameters public gameParams;
     address public owner = address(0x123);
     address public player = address(0x456);
-    
+    address public player2 = address(0x789);
+
     function setUp() public {
         vm.prank(owner);
         gameParams = new GameParameters();
-        
         vm.prank(owner);
         gameItems = new GameItems("Game Items", "GITEMS", address(gameParams));
     }
-    
+
     function test_mint_item() public {
         vm.prank(owner);
         gameItems.mint(player, 1, 10, "");
-        
-        uint256 balance = gameItems.balanceOf(player, 1);
-        assertEq(balance, 10);
+        assertEq(gameItems.balanceOf(player, 1), 10);
     }
-    
+
+    function test_revert_unauthorized_mint() public {
+        vm.prank(player);
+        vm.expectRevert();
+        gameItems.mint(player, 1, 10, "");
+    }
+
     function test_burn_item() public {
         vm.prank(owner);
         gameItems.mint(player, 1, 5, "");
-        
         vm.prank(player);
         gameItems.burn(player, 1, 2);
-        
         assertEq(gameItems.balanceOf(player, 1), 3);
     }
-    
+
+    function test_revert_unauthorized_burn() public {
+        vm.prank(owner);
+        gameItems.mint(player, 1, 5, "");
+        vm.prank(address(0x999));
+        vm.expectRevert("Not authorized");
+        gameItems.burn(player, 1, 1);
+    }
+
+    function test_approved_operator_can_burn() public {
+        vm.prank(owner);
+        gameItems.mint(player, 1, 5, "");
+        vm.prank(player);
+        gameItems.setApprovalForAll(player2, true);
+        vm.prank(player2);
+        gameItems.burn(player, 1, 2);
+        assertEq(gameItems.balanceOf(player, 1), 3);
+    }
+
     function test_craft_item() public {
         vm.prank(owner);
         gameItems.mint(player, 1, 3, "");
         vm.prank(owner);
         gameItems.mint(player, 2, 3, "");
-        
+
         uint256[] memory ingredients = new uint256[](2);
         ingredients[0] = 1;
         ingredients[1] = 2;
-        
+
         vm.prank(player);
         gameItems.craft(ingredients, 3);
-        
+
         assertEq(gameItems.balanceOf(player, 3), 1);
         assertEq(gameItems.balanceOf(player, 1), 2);
         assertEq(gameItems.balanceOf(player, 2), 2);
     }
-    
-    function test_revert_unauthorized_burn() public {
+
+    function test_revert_craft_no_ingredients() public {
+        uint256[] memory ingredients = new uint256[](0);
+        vm.prank(player);
+        vm.expectRevert("No ingredients");
+        gameItems.craft(ingredients, 3);
+    }
+
+    function test_uri_format() public view {
+        string memory uri = gameItems.uri(42);
+        assertEq(uri, "https://api.gamefi.com/items/42.json");
+    }
+
+    function testFuzz_mint_amount(uint256 amount) public {
+        amount = bound(amount, 1, 1_000_000);
         vm.prank(owner);
-        gameItems.mint(player, 1, 5, "");
-        
-        vm.prank(address(0x999));
-        vm.expectRevert("Not authorized");
-        gameItems.burn(player, 1, 1);
+        gameItems.mint(player, 1, amount, "");
+        assertEq(gameItems.balanceOf(player, 1), amount);
     }
 }
--- test/GovernanceTokenTest.t.sol
+++ test/GovernanceTokenTest.t.sol
@@ -7,21 +7,38 @@ import "../src/governance/GovernanceToken.sol";
 contract GovernanceTokenTest is Test {
     GovernanceToken public token;
     address public user = address(0x123);
-    
+
     function setUp() public {
         token = new GovernanceToken();
     }
-    
+
+    function test_initialSupply() public view {
+        assertEq(token.totalSupply(), 1_000_000 * 10 ** 18);
+    }
+
+    function test_transfer() public {
+        token.transfer(user, 100 ether);
+        assertEq(token.balanceOf(user), 100 ether);
+    }
+
     function test_delegate() public {
         token.delegate(user);
         assertEq(token.delegates(address(this)), user);
     }
-    
+
+    function test_voting_power_after_delegate() public {
+        token.transfer(user, 500 ether);
+        vm.prank(user);
+        token.delegate(user);
+        vm.roll(block.number + 1);
+        assertEq(token.getVotes(user), 500 ether);
+    }
+
     function test_permit() public {
         uint256 privateKey = 0x123;
         address owner = vm.addr(privateKey);
         token.transfer(owner, 1000 ether);
-        
+
         uint256 deadline = block.timestamp + 1 days;
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(
             privateKey,
@@ -42,8 +59,16 @@ contract GovernanceTokenTest is Test {
                 )
             )
         );
-        
         token.permit(owner, user, 100 ether, deadline, v, r, s);
         assertEq(token.allowance(owner, user), 100 ether);
+    }
+
+    function testFuzz_votingPower(uint256 amount) public {
+        amount = bound(amount, 1, 1_000_000 * 10 ** 18);
+        token.transfer(user, amount);
+        vm.prank(user);
+        token.delegate(user);
+        vm.roll(block.number + 1);
+        assertEq(token.getVotes(user), amount);
     }
 }
